8e4096f84575fbc98ba3f4920b79f748d2d2796f
Sayeedur <sayeedur.rahman@redoq.co.uk>
2025-02-11 17:11:53 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_days/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_days
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Image.asset('assets/image/login_image.png',fit: BoxFit.cover),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+              'Welcome',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32,vertical: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Username',
+                    labelText: 'Username'
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      labelText: 'Password'
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    minimumSize: Size(150, 40),
+                    backgroundColor: Colors.deepPurple,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Text('Login'),
+                  onPressed: (){}, 
+                )
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
