8265b80d86ff34f46ccf9e007d2b782315111b7d
Sayeedur <sayeedur.rahman@redoq.co.uk>
2025-02-14 13:57:02 +0530

    feat: Implement item display with list view in HomePage
    This commit introduces the display of items in the `HomePage` using a `ListView` and creates a new `ItemWidget` to represent each item. Key changes include:
    
    -   Creation of `catalog.dart` to define `CatalogModel` and `Item` classes.
    - Added a `Item` class with `id`, `name`, `desc`, `price`, `color`, `image`.
    -   Added `CatalogModel` class with a list of `items`.
    -   Creation of `item_widget.dart` to define the `ItemWidget` that represents an individual item.
    -   Implementation of `ItemWidget` to display the item's image, name, description, and price.
    -   Updated `home_page.dart` to use `ListView.builder` to dynamically create a list of `ItemWidget` instances.
    -   Used dummy list of items from `CatalogModel` in the `HomePage`.
    -   Added `ListTile` and `Card` widgets for each item.
    - Added `Image.network` to get the item image from the network.
    - Added a list of `items` inside the `CatalogModel` class.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_days/models/catalog.dart';
 import 'package:flutter_days/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(100, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
         centerTitle: true,
       ),
-      body: Center(
-          child: Text(
-              'Welcome to the HomePage'
-          )
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+            itemBuilder: (context, index){
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }
+        ),
       ),
       drawer: MyDrawer(),
     );
